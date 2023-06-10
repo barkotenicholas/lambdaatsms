ed497f93fe0109584c894fc96b0bb2d4dfeb7fb9
barkotenicholas <barkotenicholas@gmail.com>
2023-06-10 14:49:07 +0300

    Added new function

--- barkote_send_sms.py
+++ barkote_send_sms.py
@@ -4,5 +4,5 @@ def barkote_send_sms(event,context):
     #ToDo
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': "json.dumps('Hello from Lambda!')"
     }
