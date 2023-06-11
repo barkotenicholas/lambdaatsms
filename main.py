c3309c16bcaa8ea81418313f2a8a9197cdce88ea
barkotenicholas <barkotenicholas@gmail.com>
2023-06-11 16:09:02 +0300

    Added new code

--- main.py
+++
@@ -1,7 +0,0 @@
-from africas_talking import AfricasTalking
-from send_sms import SendSms
-class Main(AfricasTalking,SendSms):
-    def __init__(self):
-        super().__init__()
-        SendSms().__init__(self)
-        self.send_sms = self.send_sms("Today","Phone")
