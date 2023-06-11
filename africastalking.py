19581abb08de883172837f4f0ec1320ed3ee4cff
barkotenicholas <barkotenicholas@gmail.com>
2023-06-11 15:01:26 +0300

    Added removed ucecssesary code

--- africastalking.py
+++ africastalking.py
@@ -5,4 +5,4 @@ class AfricasTalking:
         # Initialize SDK
         username = "awssms" 
         api_key = "394957400d15920d7c16a7256387bec89219e3d78e7a40379378916367c7df1f"
-        self.africastalking = africastalking.initialize(username, api_key)
+        africastalking.initialize(username, api_key)
