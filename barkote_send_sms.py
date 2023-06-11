6e030871fa8b595d5dd59332a7cad6f7b63bd1ff
barkotenicholas <barkotenicholas@gmail.com>
2023-06-11 23:00:00 +0300

    updated github actions

--- barkote_send_sms.py
+++ barkote_send_sms.py
@@ -2,11 +2,7 @@
 import africastalking as at
 def barkote_send_sms(event,context):
  
-    username = "awssms" 
-    api_key = "394957400d15920d7c16a7256387bec89219e3d78e7a40379378916367c7df1f"
-    at.initialize(username, api_key)
- 
-    sms = at.SMS
+
 
     return {
         'message':"response"
