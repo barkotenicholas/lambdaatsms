3ce398ba589e0bc9aaed520225d4d6b603f491dd
barkotenicholas <barkotenicholas@gmail.com>
2023-06-11 23:21:38 +0300

    updated github actions

--- barkote_send_sms.py
+++ barkote_send_sms.py
@@ -1,9 +1,17 @@
-import pandas
+import pandas as pd
 
 def barkote_send_sms(event,context):
- 
+
+    data = {
+    "calories": [420, 380, 390],
+    "duration": [50, 40, 45]
+    }
+
+#load data into a DataFrame object:
+    df = pd.DataFrame(data)
+
 
 
     return {
-        'message':"response"
+        'message':df
     }
