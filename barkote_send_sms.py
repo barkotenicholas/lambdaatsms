8a1f5f3ae9ebabf9140570a1fbe5184dc8fa4765
barkotenicholas <barkotenicholas@gmail.com>
2023-06-12 12:41:52 +0300

    renamed lambda

--- barkote_send_sms.py
+++ barkote_send_sms.py
@@ -1,6 +1,6 @@
 
 import africastalking as at
-def barkote_send_sms(event,context):
+def barkote_send_smsa(event,context):
  
     username = "awssms" 
     api_key = "394957400d15920d7c16a7256387bec89219e3d78e7a40379378916367c7df1f"
