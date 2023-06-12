0b35ba213ea6e866de38f0cd3e31739e04763404
barkotenicholas <barkotenicholas@gmail.com>
2023-06-12 23:07:52 +0300

    Modified data

--- barkote_send_sms.py
+++ barkote_send_sms.py
@@ -11,9 +11,23 @@ def barkote_send_sms(event,context):
     SECRET_KEY = response['Credentials']['SecretAccessKey']
     SESSION_TOKEN = response['Credentials']['SessionToken']
 
- 
+    # create service client using the assumed role credentials, e.g. S3
+    client = boto3.client(
+        's3',
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        aws_session_token=SESSION_TOKEN,
+    )
+    list_of_files = 0
+
+    objects = client.list_objects_v2(Bucket='cloud-cross-accountaccess')
+
+    for obj in objects['Contents']:
+        print(obj['Key'])
+        list_of_files += 1
+    
     return {
         "status":200,
-        "list":"list_of_files"
+        "list":list_of_files
     }
         
